9497f67f660a5cbf4039657ca351d997e65d6c55
Thomas Menard <menardorama@gmail.com>
2026-09-12 22:57:12 +0200

    fix: marge 2mm et nom PPE - Photos (retour utilisateur)

--- app/src/main/java/com/tmenard/planchecontact/pdf/GridCalculator.kt
+++ app/src/main/java/com/tmenard/planchecontact/pdf/GridCalculator.kt
@@ -21,7 +21,7 @@ data class SheetLayout(
 )
 
 object GridCalculator {
-    const val MARGIN_MM = 4f
+    const val MARGIN_MM = 2f
     const val GAP_MM = 2f
     const val MM_TO_PT = 72f / 25.4f
     const val PRINT_DPI = 300
